3972e62bde953f48728ff1eff9f10a42af30c6f2
alla57 <a.boumediene01@gmail.com>
2024-12-11 17:44:29 +0000

    removed unused files

--- ansible_install.sh
+++
@@ -1,4 +0,0 @@
-# install ansible
-sudo apt update
-sudo apt install python3-pip
-sudo pip3 install ansible
--- vm-config.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt install -y vim
-sudo apt install git
-sudo apt install -y pipx
-pipx install --include-deps ansible
-pipx install linode-cli
-
-
-install linode-cli
